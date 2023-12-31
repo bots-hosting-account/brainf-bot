a5bbee2f91960c9810a7351701e481c31503beec
bots-hosting-account <155102703+bots-hosting-account@users.noreply.github.com>
2023-12-31 14:12:19 -0500

    Fix directory

--- get_files.sh
+++ get_files.sh
@@ -1,5 +1,5 @@
 for filename in $(cat files_to_update.txt);
 do
   echo Reading $filename
-  curl https://raw.githubusercontent.com/bots-hosting-account/brainf-bot/master/$filename -o bot/$filename
+  curl https://raw.githubusercontent.com/bots-hosting-account/brainf-bot/master/bot/$filename -o bot/$filename
 done
